f9f8238c51459db811b57c17fce429c5d3c3e3d5
zte-chenjun <228379165@qq.com>
2018-02-19 16:03:43 +0800

    TESTaha

--- src/main/java/com/dreambird/javaBasics/Thread/SwingAndThread.java
+++ src/main/java/com/dreambird/javaBasics/Thread/SwingAndThread.java
@@ -7,7 +7,7 @@ import javax.swing.*;
 
 public class SwingAndThread extends JFrame {
 	/**
-	 * TEST aha
+	 * TEST aha a
 	 */
 	private static final long serialVersionUID = 1L;
 	private JLabel jl = new JLabel(); // ÉùÃ÷JLabel¶ÔÏó
